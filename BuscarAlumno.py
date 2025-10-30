736989d94dfa5bc972db4d2acb3c5f1892d99a95
Fabricio Alonso Lanche Pacsi <fabricio.lanche@utec.edu.pe>
2025-10-30 18:18:13 +0000

    Refactor lambda_handler to simplify event parsing and response structure

--- BuscarAlumno.py
+++ BuscarAlumno.py
@@ -1,31 +1,20 @@
-import json
 import boto3
 
 def lambda_handler(event, context):
-    # Si el evento viene desde API Gateway, parsea el body
-    if 'body' in event:
-        try:
-            body = json.loads(event['body']) if isinstance(event['body'], str) else event['body']
-        except json.JSONDecodeError:
-            body = {}
-    else:
-        # Si se ejecuta directamente desde la consola Lambda
-        body = event
-
-    tenant_id = body['tenant_id']
-    alumno_id = body['alumno_id']
-
+    # Entrada (json)
+    tenant_id = event['tenant_id']
+    alumno_id = event['alumno_id']
+    # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
-
     response = table.get_item(
         Key={
             'tenant_id': tenant_id,
             'alumno_id': alumno_id
         }
     )
-
+    # Salida (json)
     return {
         'statusCode': 200,
-        'body': json.dumps(response, ensure_ascii=False)
+        'response': response
     }
